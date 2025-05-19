9ca5a4be423725e49b9746d9a04474f1b82db8cd
gaurav-grip <gaurav.jaiman@gripinvest.in>
2025-05-19 17:43:54 +0530

    Enable CORS globally in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // or 'https://main.d1j14942k7p89p.amplifyapp.com'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
